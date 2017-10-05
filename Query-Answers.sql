9a3e52afb730bd0e025bb5dd9f0769b94015b1f8
Ian Lefcourte <ianlefcourte@gmail.com>
2017-10-04 20:11:16 -0700

    Update Query-Answers.sql

--- Query-Answers.sql
+++ Query-Answers.sql
@@ -63,3 +63,29 @@ WHERE Director != "John Lasseter";
 SELECT *
 FROM Movies
 WHERE Title LIKE "%WALL%";
+
+-- CH3 - Queries with constraints (Pt. 2)
+
+-- List all directors of Pixar movies (alphabetically), without duplicates
+SELECT DISTINCT Director
+FROM Movies
+ORDER BY Director;
+
+-- List the last four Pixar movies released (ordered from most recent to least)
+SELECT *
+FROM Movies
+ORDER BY Year DESC
+LIMIT 4;
+
+-- List the first five Pixar movies sorted alphabetically
+SELECT *
+FROM Movies
+ORDER BY Title ASC
+LIMIT 5;
+
+-- List the next five Pixar movies sorted alphabetically
+SELECT *
+FROM Movies
+ORDER BY Title ASC
+LIMIT 5
+OFFSET 5;
