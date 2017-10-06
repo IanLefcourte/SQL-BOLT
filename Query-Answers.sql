8955dd4ce1df363619993185f0d61220f0c6d5a2
Ian Lefcourte <ianlefcourte@gmail.com>
2017-10-05 19:54:29 -0700

    Update Query-Answers.sql

--- Query-Answers.sql
+++ Query-Answers.sql
@@ -90,5 +90,37 @@ ORDER BY Title ASC
 LIMIT 5
 OFFSET 5;
 
--- CH5 - xxx
+-- CH5 - Review Simple SELECT Queries
+
+-- List all the Canadian cities and their populations 
+SELECT *
+FROM North_american_cities
+WHERE Country LIKE "Canada";
+
+-- Order all the cities in the United States by their latitude from north to south
+SELECT *
+FROM North_american_cities
+WHERE Country = "United States"
+ORDER BY Latitude DESC;
+
+-- List all the cities west of Chicago, ordered from west to east
+SELECT *
+FROM North_american_cities
+WHERE Longitude < -87.69
+ORDER BY Longitude ASC;
+
+-- List the two largest cities in Mexico (by population)
+SELECT *
+FROM North_american_cities
+WHERE Country LIKE "Mexico"
+ORDER BY Population DESC
+LIMIT 2;
+
+-- List the third and fourth largest cities (by population) in the United States and their population
+SELECT *
+FROM North_american_cities
+WHERE Country LIKE "United States"
+ORDER BY Population DESC
+LIMIT 2
+OFFSET 2;
 
