98e9624bd0ac37666be2cb56328e59f563760792
Ian Lefcourte <ianlefcourte@gmail.com>
2017-10-06 19:29:41 -0700

    Update Query-Answers.sql

--- Query-Answers.sql
+++ Query-Answers.sql
@@ -124,3 +124,21 @@ ORDER BY Population DESC
 LIMIT 2
 OFFSET 2;
 
+-- CH6 - Review Simple SELECT Queries
+
+-- Find the domestic and international sales for each movie
+SELECT Title, International_sales, Domestic_sales
+FROM Movies JOIN Boxoffice
+ON Id=Movie_id;
+
+-- Show the sales numbers for each movie that did better internationally rather than domestically
+SELECT Title, International_sales, Domestic_sales
+FROM Movies JOIN Boxoffice
+ON Id=Movie_id
+WHERE International_sales > Domestic_sales;
+
+-- List all the movies by their ratings in descending order
+SELECT Title, Rating
+FROM Movies JOIN Boxoffice
+ON Id=Movie_id
+ORDER BY Rating Desc;
