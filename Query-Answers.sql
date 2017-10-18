592effb40e677c3a454927e8b1879c7fdaa3f83c
Ian Lefcourte <ianlefcourte@gmail.com>
2017-10-17 22:24:44 -0700

    Update Query-Answers.sql

--- Query-Answers.sql
+++ Query-Answers.sql
@@ -241,3 +241,12 @@ FROM Movies
 LEFT JOIN Boxoffice ON Id = Movie_ID
 GROUP BY Director;
 
+-- CH13 - Inserting rows
+
+-- Add the studio's new production, Toy Story 4 to the list of movies (you can use any director)
+INSERT INTO Movies,
+VALUES (4, "Toy Story 4", "John Lasseter", 2017, 123);
+
+-- Toy Story 4 has been released to critical acclaim! It had a rating of 8.7, and made 340 million domestically and 270 million internationally. Add the record to the  BoxOffice table. 
+INSERT INTO Boxoffice
+VALUES (4, 8.7, 340000000, 270000000);
