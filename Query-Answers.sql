697f6c06548d445226a81bdcf4b154630c2a767c
Ian Lefcourte <ianlefcourte@gmail.com>
2017-10-23 09:21:06 -0700

    Update Query-Answers.sql

--- Query-Answers.sql
+++ Query-Answers.sql
@@ -277,3 +277,14 @@ WHERE Year < 2005;
 -- Andrew Stanton has also left the studio, so please remove all movies directed by him.
 DELETE FROM Movies
 WHERE Director = "Andrew Stanton";
+
+-- CH16 - Creating Tables
+-- Create a new table named Database with the following columns:
+-- 1. Name A string (text) describing the name of the database
+-- 2. Version A number (floating point) of the latest version of this database
+-- 3. Download_count An integer count of the number of times this database was downloaded
+-- This table has no constraints.
+CREATE TABLE Database (
+    Name TEXT,
+    Version FLOAT,
+    Download_Count INTEGER);
