7a04adab32b7b6eb3ce480cff4942bc502ff41b1
Ian Lefcourte <ianlefcourte@gmail.com>
2017-10-24 20:11:36 -0700

    Update Query-Answers.sql

--- Query-Answers.sql
+++ Query-Answers.sql
@@ -289,3 +289,13 @@ CREATE TABLE Database (
     Name TEXT,
     Version FLOAT,
     Download_Count INTEGER);
+    
+-- CH17 - Altering Tables
+
+-- Add a column named Aspect_ratio with a FLOAT data type to store the aspect-ratio each movie was released in.
+ALTER TABLE Movies
+  ADD COLUMN Aspect_ratio FLOAT DEFAULT 3;
+  
+-- Add another column named Language with a TEXT data type to store the language that the movie was released in. Ensure that the default for this language is English.
+ALTER TABLE Movies
+  ADD COLUMN Language TEXT DEFAULT "English";
